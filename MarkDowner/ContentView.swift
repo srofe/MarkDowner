97f09035209edb266da8e2e1b75fc6946c3b574e
Simon Rofe <simon.rofe@ieee.org>
2022-05-23 10:40:50 +1000

    Add toolbar for view settings
    Doesn't do anything yet, just enables selection of view mode between
    showing web view, html code or turning the preview off.

--- MarkDowner/ContentView.swift
+++ MarkDowner/ContentView.swift
@@ -7,8 +7,15 @@
 
 import SwiftUI
 
+enum PreviewState {
+    case web
+    case code
+    case off
+}
+
 struct ContentView: View {
     @Binding var document: MarkDownerDocument
+    @State private var previewState = PreviewState.web
 
     var body: some View {
         HSplitView {
@@ -18,6 +25,20 @@ struct ContentView: View {
                 .frame(minWidth: 200)
         }
         .frame(minWidth: 400, idealWidth: 600, maxWidth: .infinity, minHeight: 300, idealHeight: 400, maxHeight: .infinity)
+        .toolbar {
+            ToolbarItem {
+                Picker("", selection: $previewState) {
+                    Image(systemName: "network")
+                        .tag(PreviewState.web)
+                    Image(systemName: "chevron.left.forwardslash.chevron.right")
+                        .tag(PreviewState.code)
+                    Image(systemName: "nosign")
+                        .tag(PreviewState.off)
+                }
+                .pickerStyle(.segmented)
+                .help("Hide preview, show HTML or web view")
+            }
+        }
     }
 }
 
