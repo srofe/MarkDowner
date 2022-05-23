ac40e4b5193602b42e76c1edd6697d6d9599fddb
Simon Rofe <simon.rofe@ieee.org>
2022-05-23 11:58:35 +1000

    Add stylesheet enumeration

---
+++ MarkDowner/StyleSheets/StyleSheet.swift
@@ -0,0 +1,16 @@
+//
+//  StyleSheet.swift
+//  MarkDowner
+//
+//  Created by Simon Rofe on 23/5/2022.
+//
+
+import Foundation
+
+enum StyleSheet: String, CaseIterable {
+    case raywenderlich = "Ray Wenderlich"
+    case github = "github"
+    case lopash = "Lopash"
+    case darksolarised = "Dark Solarised"
+    case ulysses = "Ulysses"
+}
